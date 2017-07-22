c721c8972534f87d6f0b8cdad753d46a1bcf868d
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-22 16:32:52 -0400

    Add users data

---
+++ client/src/data/users.js
@@ -0,0 +1,256 @@
+export const users = [
+  {
+    "id":1,
+    "first_name":"Alejandra",
+    "last_name":"Nicolas",
+    "portfolios":[
+      {
+        "name":"Harum Rem Culpa",
+        "cash":95482.82,
+        "market_value":784050.05,
+        "total_value":879532.87,
+        "return_value":7.7953
+      },
+      {
+        "name":"Non Quo Ducimus",
+        "cash":24131.12,
+        "market_value":967257.78,
+        "total_value":991388.9,
+        "return_value":8.9139
+      },
+      {
+        "name":"Corporis Necessitatibus Ipsam",
+        "cash":19782.32,
+        "market_value":901257.89,
+        "total_value":921040.21,
+        "return_value":8.2104
+      },
+      {
+        "name":"Ipsam Mollitia Dolores",
+        "cash":30034.45,
+        "market_value":1255653.4,
+        "total_value":1285687.85,
+        "return_value":11.8569
+      },
+      {
+        "name":"Dolorum Est Voluptas",
+        "cash":72222.62,
+        "market_value":1367533.1,
+        "total_value":1439755.72,
+        "return_value":13.3976
+      }
+    ]
+  }
+  {
+    "id":2,
+    "first_name":"Katelyn",
+    "last_name":"Gibson",
+    "portfolios":[
+      {
+        "name":"Dolorem Possimus Ut",
+        "cash":71053.08,
+        "market_value":1166851.85,
+        "total_value":1237904.93,
+        "return_value":11.379
+      },
+      {
+        "name":"Commodi Hic Consequatur",
+        "cash":11128.13,
+        "market_value":920694.93,
+        "total_value":931823.06,
+        "return_value":8.3182
+      },
+      {
+        "name":"Expedita Velit Dicta",
+        "cash":93254.92,
+        "market_value":1005090.57,
+        "total_value":1098345.49,
+        "return_value":9.9835
+      },
+      {
+        "name":"Sint Delectus Maxime",
+        "cash":3821.95,
+        "market_value":1296767.41,
+        "total_value":1300589.36,
+        "return_value":12.0059
+      },
+      {
+        "name":"Laborum Consectetur Iusto",
+        "cash":49782.0,
+        "market_value":1224167.11,
+        "total_value":1273949.11,
+        "return_value":11.7395
+      }
+    ]
+  },
+  {
+    "id":3,
+    "first_name":"Miracle",
+    "last_name":"Huels",
+    "portfolios":[
+      {
+        "name":"Numquam Possimus Nihil",
+        "cash":73304.11,
+        "market_value":850798.69,
+        "total_value":924102.8,
+        "return_value":8.241
+      },
+      {
+        "name":"Doloremque Quaerat Ut",
+        "cash":97914.28,
+        "market_value":1295777.04,
+        "total_value":1393691.32,
+        "return_value":12.9369
+      },
+      {
+        "name":"Ex Error Eum",
+        "cash":43610.4,
+        "market_value":995520.69,
+        "total_value":1039131.09,
+        "return_value":9.3913
+      },
+      {
+        "name":"Vitae Id At",
+        "cash":18124.72,
+        "market_value":1068439.62,
+        "total_value":1086564.34,
+        "return_value":9.8656
+      },
+      {
+        "name":"Rem Quod Molestiae",
+        "cash":72579.09,
+        "market_value":1127801.97,
+        "total_value":1200381.06,
+        "return_value":11.0038
+      }
+    ]
+  },
+  {
+    "id":4,
+    "first_name":"Casimer",
+    "last_name":"Swift",
+    "portfolios":[
+      {
+        "name":"Nam Aut Reiciendis",
+        "cash":88465.71,
+        "market_value":960709.62,
+        "total_value":1049175.33,
+        "return_value":9.4918
+      },
+      {
+        "name":"Praesentium Quidem Consectetur",
+        "cash":74303.82,
+        "market_value":1014736.5,
+        "total_value":1089040.32,
+        "return_value":9.8904
+      },
+      {
+        "name":"Voluptatem Culpa Est",
+        "cash":26614.3,
+        "market_value":1216160.49,
+        "total_value":1242774.79,
+        "return_value":11.4277
+      },
+      {
+        "name":"Dolores Voluptatum Laboriosam",
+        "cash":80185.9,
+        "market_value":1222368.14,
+        "total_value":1302554.04,
+        "return_value":12.0255
+      },
+      {
+        "name":"At Animi Id",
+        "cash":60972.12,
+        "market_value":1511507.87,
+        "total_value":1572479.99,
+        "return_value":14.7248
+      }
+    ]
+  },
+  {
+    "id":5,
+    "first_name":"Marvin",
+    "last_name":"Parker",
+    "portfolios":[
+      {
+        "name":"Sit Repudiandae Vitae",
+        "cash":77452.0,
+        "market_value":1396911.64,
+        "total_value":1474363.64,
+        "return_value":13.7436
+      },
+      {
+        "name":"Alias In Suscipit",
+        "cash":6036.19,
+        "market_value":669710.11,
+        "total_value":675746.3,
+        "return_value":5.7575
+      },
+      {
+        "name":"Omnis Sit Vero",
+        "cash":64975.64,
+        "market_value":1478931.68,
+        "total_value":1543907.32,
+        "return_value":14.4391
+      },
+      {
+        "name":"Ad Sunt Ipsum",
+        "cash":14633.85,
+        "market_value":1224041.01,
+        "total_value":1238674.86,
+        "return_value":11.3867
+      },
+      {
+        "name":"Dolor Ut Consequatur",
+        "cash":78071.53,
+        "market_value":1048751.89,
+        "total_value":1126823.42,
+        "return_value":10.2682
+      }
+    ]
+  },
+  {
+    "id":6,
+    "first_name":"Blake",
+    "last_name":"Hilll",
+    "portfolios":[
+      {
+        "name":"Quia Nam Culpa",
+        "cash":9435.46,
+        "market_value":1224588.67,
+        "total_value":1234024.13,
+        "return_value":11.3402
+      },
+      {
+        "name":"Optio Sit Animi",
+        "cash":17111.98,
+        "market_value":740771.65,
+        "total_value":757883.63,
+        "return_value":6.5788
+      },
+      {
+        "name":"At Maxime Quidem",
+        "cash":77583.23,
+        "market_value":1128374.41,
+        "total_value":1205957.64,
+        "return_value":11.0596
+      },
+      {
+        "name":"Molestiae Vel Dignissimos",
+        "cash":3042.71,
+        "market_value":1162466.24,
+        "total_value":1165508.95,
+        "return_value":10.6551
+      },
+      {
+        "name":"Mollitia Necessitatibus Adipisci",
+        "cash":17382.53,
+        "market_value":1003730.54,
+        "total_value":1021113.07,
+        "return_value":9.2111
+      }
+    ]
+  }
+]
+
+export default users;
