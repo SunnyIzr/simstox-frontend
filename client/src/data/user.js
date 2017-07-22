d013bf2a7494cd88aca06b1bb354741849ac88c0
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-22 16:26:35 -0400

    Add user data

---
+++ client/src/data/user.js
@@ -0,0 +1,44 @@
+export const user = {
+  "id":1,
+  "first_name":"Alejandra",
+  "last_name":"Nicolas",
+  "portfolios":[
+    {
+      "name":"Harum Rem Culpa",
+      "cash":95482.82,
+      "market_value":784050.05,
+      "total_value":879532.87,
+      "return_value":7.7953
+    },
+    {
+      "name":"Non Quo Ducimus",
+      "cash":24131.12,
+      "market_value":967257.78,
+      "total_value":991388.9,
+      "return_value":8.9139
+    },
+    {
+      "name":"Corporis Necessitatibus Ipsam",
+      "cash":19782.32,
+      "market_value":901257.89,
+      "total_value":921040.21,
+      "return_value":8.2104
+    },
+    {
+      "name":"Ipsam Mollitia Dolores",
+      "cash":30034.45,
+      "market_value":1255653.4,
+      "total_value":1285687.85,
+      "return_value":11.8569
+    },
+    {
+      "name":"Dolorum Est Voluptas",
+      "cash":72222.62,
+      "market_value":1367533.1,
+      "total_value":1439755.72,
+      "return_value":13.3976
+    }
+  ]
+}
+
+export default user;
