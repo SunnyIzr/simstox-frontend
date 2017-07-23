3f8cc6ed49d79a524abdc118e1a89dcbee62063c
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-23 19:44:39 -0400

    Pull out LoginForm into its own component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,56 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/Home.css'
+import LoginForm from './LoginForm'
 
 class Home extends Component {
-  constructor(){
-    super()
-    this.state={
-      username: '',
-      password: ''
-    }
-    this.handleUsernameUpdate = this.handleUsernameUpdate.bind(this)
-    this.handlePasswordUpdate = this.handlePasswordUpdate.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleUsernameUpdate(e){
-    this.setState({username: e.target.value})
-  }
-
-  handlePasswordUpdate(e){
-    this.setState({password: e.target.value})
-  }
-
-  handleSubmit(e){
-    e.preventDefault()
-    this.props.login(this.state)
-  }
-
   render(){
     return(
       <div>
         <h2 className='home-message'> Welcome to the simstox </h2>
         <div className="row">
-          <form className="login-form col s12" onSubmit={this.handleSubmit}>
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="username" type="text" className="validate" onChange={this.handleUsernameUpdate} placeholder="Username" />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="password" type="password" className="validate" onChange={this.handlePasswordUpdate} placeholder="Password" />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="input-field col s12">
-                <button className="btn waves-effect waves-light login-btn" type="submit" name="action">Login
-                </button>
-              </div>
-            </div>
-          </form>
+          <LoginForm login={this.props.login}/>
         </div>
       </div>
     )
