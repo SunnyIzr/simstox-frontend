607595fe993d47edac4227cc0c53c0240b8379c3
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-23 17:56:06 -0400

    Create basic login form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import '../styles/Home.css'
 
 class Home extends Component {
   constructor(){
     super()
-    this.state ={
-      users: []
+    this.state={
+      username: '',
+      password: ''
     }
+    this.handleUsernameUpdate = this.handleUsernameUpdate.bind(this)
+    this.handlePasswordUpdate = this.handlePasswordUpdate.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-  componentDidMount(){
-    fetch('http://localhost:3001/users')
-      .then(function(response) {
-        return response.text()
-      }).then(function(body) {
-        this.setState({users: JSON.parse(body)})
-      }.bind(this))
+
+  handleUsernameUpdate(e){
+    this.setState({username: e.target.value})
   }
+
+  handlePasswordUpdate(e){
+    this.setState({password: e.target.value})
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    this.props.login(this.state)
+  }
+
   render(){
-    let { users } = this.state
     return(
       <div>
-        <h2> Welcome to the simstox </h2>
-        <ul className="collection">
-          {users.map( (user, i) => 
-            <Link to='/user' key={i} onClick={(e) => this.props.login(user) }>
-              <li className="collection-item" >
-                  { user.first_name } { user.last_name }
-              </li>
-            </Link>
-          )}
-        </ul>
+        <h2 className='home-message'> Welcome to the simstox </h2>
+        <div className="row">
+          <form className="login-form col s12" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="username" type="text" className="validate" onChange={this.handleUsernameUpdate} placeholder="Username" />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="password" type="password" className="validate" onChange={this.handlePasswordUpdate} placeholder="Password" />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <button className="btn waves-effect waves-light login-btn" type="submit" name="action">Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
