eb7aeec37f5a6f448cca28985b9488bae838535b
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-25 13:29:24 -0400

    Convert Home into functional stateless component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import '../styles/Home.css'
 import LoginForm from './LoginForm'
 
-class Home extends Component {
-  render(){
-    return(
-      <div>
-        <h2 className='home-message'> Welcome to the simstox </h2>
-        <div className="row">
-          <LoginForm login={this.props.login}/>
-        </div>
-      </div>
-    )
-  }
-}
+const Home = ({ login }) => (
+  <div>
+    <h2 className='home-message'> Welcome to the simstox </h2>
+    <div className="row">
+      <LoginForm login={login}/>
+    </div>
+  </div>
+)
 
 export default Home;
