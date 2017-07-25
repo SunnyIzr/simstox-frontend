9288b640bed75dd36d5c1d7f7f1ee6b8441051cb
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-25 10:25:24 -0400

    Change User into a presentational stateless functional component

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -3,24 +3,19 @@ import '../styles/User.css'
 import Portfolios from './Portfolios'
 import { Link } from 'react-router-dom'
 
-class User extends Component {
-  render() {
-    let { first_name, portfolios } = this.props.user
-    return (
-      <div>
-        <div className='greet'>
-          <h2> Welcome {first_name}. </h2>
-          <h4> It's a great day to trade. </h4>
-        </div>
-        <div className='user-btn-container center'>
-          <Link to="/trade" className="waves-effect waves-light btn">Trade</Link>
-        </div>
-        <div className='clearfix'></div>
-        <Portfolios portfolios={portfolios} />
-      </div>
 
-    )
-  }
-}
+const User = ( {firstName, portfolios }) => (
+  <div>
+    <div className='greet'>
+      <h2> Welcome {firstName}. </h2>
+      <h4> It's a great day to trade. </h4>
+    </div>
+    <div className='user-btn-container center'>
+      <Link to="/trade" className="waves-effect waves-light btn">Trade</Link>
+    </div>
+    <div className='clearfix'></div>
+    <Portfolios portfolios={portfolios} />
+  </div>
+)
 
 export default User;
