41f56dc775a719c232199d1feaf30d05c7e725ed
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-26 13:56:18 -0400

    Set up node server to serve correct path for static assets

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var PORT = process.env.PORT || 3001;
 var app = express();
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+app.use(express.static('./client/build'))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+
 });
 
 app.listen(PORT, function() {
