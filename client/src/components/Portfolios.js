a69091297d083f202db6bd98fe3ad5560f2a5eb9
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-27 15:14:32 -0400

    Add empty state for 0 portfolios

--- client/src/components/Portfolios.js
+++ client/src/components/Portfolios.js
@@ -3,25 +3,31 @@ import PortfolioRowContainer from '../containers/PortfolioRowContainer'
 
 const Portfolios = ({ portfolios }) => (
   <div>
-    <h5>Portfolios</h5>
-    <table className='responsive-table centered highlight bordered'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Cash</th>
-          <th>Market Value</th>
-          <th>Total Value</th>
-          <th>Total P&L</th>
-          <th>Return</th>
-        </tr>
-      </thead>
-      <tbody>
-        { portfolios.map( (portfolio, i) =>
-            <PortfolioRowContainer portfolio={portfolio} key={i} />
-          )
-        }
-      </tbody>
-    </table>
+    { portfolios.length === 0 ? (
+      <h5>You have no portfolios. Create one!</h5>
+    ) : (
+      <div>
+        <h5>Portfolios</h5>
+        <table className='responsive-table centered highlight bordered'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Cash</th>
+              <th>Market Value</th>
+              <th>Total Value</th>
+              <th>Total P&L</th>
+              <th>Return</th>
+            </tr>
+          </thead>
+          <tbody>
+            { portfolios.map( (portfolio, i) =>
+                <PortfolioRowContainer portfolio={portfolio} key={i} />
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )}
   </div>
 )
 
